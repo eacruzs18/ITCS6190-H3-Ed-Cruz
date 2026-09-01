454a83656a28f5d36faa741af09bf25ab91e2890
Ed Cruz <eacruzs18@gmail.com>
2026-09-01 17:32:43 -0400

    update taml file

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return f'Hello World! I have been seen {count} times.\n'
+    return 'Hello World! I have been seen {} times.\n'.format(count)
